24977e0935a707c5111845d484be0a9dbcc429f6
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-25 03:01:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let prices=document.getElementsByClassName("price");
+	let n=prices.length;
+	let sum=0;
+	for(let i=0;i<n;i++)
+		{
+			let price=(Number)(prices.item(i).innerText);
+			sum+=price;
+        }
+	
+	return sum;
   
 };
 
